351de9c482b59ae6f74cd5758d6b016a32f28dd9
Linus Lewandowski <linus.lewandowski@netguru.co>
2019-02-20 23:42:11 +0100

    Move PhoneNumberField to fields.py.

---
+++ django_modular_user/fields.py
@@ -0,0 +1,16 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+phone_regex = RegexValidator(regex=r'^\+\d{8,15}$', message=_("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
+
+
+class PhoneNumberField(models.CharField):
+	default_validators = [phone_regex]
+	description = _("Phone number")
+
+	def __init__(self, *args, **kwargs):
+		kwargs.setdefault('max_length', 20)
+		super().__init__(*args, **kwargs)
+
--- django_modular_user/user_phone_number.py
+++ django_modular_user/user_phone_number.py
@@ -1,7 +1,8 @@
 from django.conf import settings
-from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from django_modular_user.fields import PhoneNumberField
 
 try:
 	UNIQUE = settings.USER_PHONE_NUMBER_UNIQUE
@@ -10,21 +11,12 @@ except AttributeError:
 
 
 def normalize_phone_number(phone_number):
+	"""
+	Normalize the phone number by removing all spaces and dashes.
+	"""
 	if phone_number is None:
 		return None
 	return phone_number.strip().replace(' ', '').replace('-', '')
-
-
-phone_regex = RegexValidator(regex=r'^\+\d{8,15}$', message=_("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
-
-
-class PhoneNumberField(models.CharField):
-	default_validators = [phone_regex]
-	description = _("Phone number")
-
-	def __init__(self, *args, **kwargs):
-		kwargs.setdefault('max_length', 20)
-		super().__init__(*args, **kwargs)
 
 
 class PhoneNumberMixin(models.Model):
